1819ae51e99e0ba914a9821ee07cac2213f673ac
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2026-02-09 12:04:20 +0000

    feat: implemnt crud operations for users

--- src/app/core/data-store.service.ts
+++ src/app/core/data-store.service.ts
@@ -1,16 +1,50 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.development';
+export interface User {
+  "id": number,
+  "firstName": string,
+  "lastName": string,
+  "email": string,
+  "password": string
+}
+
+export interface UserDTO{ 
+  "id": number,
+  "firstName": string,
+  "lastName": string,
+  "email": string,
+
+}
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataStoreService {
 
-  constructor() { }
-
-
-  addUser(){
-    
+  constructor(private http: HttpClient) {
   }
 
+
+
+  getUsers(): Observable<UserDTO[]> {
+    return this.http.get<UserDTO[]>(`${environment.jsonServerUrl}/users`);
+  }
+  getUserById(userId:number): Observable<UserDTO[]> {
+    return this.http.get<UserDTO[]>(`${environment.jsonServerUrl}/users/${userId}`);
+  }
+  addUser(user: User): Observable<UserDTO> {
+    return this.http.post<UserDTO>(`${environment.jsonServerUrl}/users`, user);
+  }
+
+  updateUser(userId:number,user: User): Observable<UserDTO> {
+    return this.http.patch<UserDTO>(`${environment.jsonServerUrl}/users/${userId}`, user);
+  }
+
+  deleteUser(userId: number): Observable<any> {
+    return this.http.delete<any>(`${environment.jsonServerUrl}/users/${userId}`);
+  }
 
 }
