9e5599a2d6695760633d0e70d405501733a26288
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2026-02-10 14:54:37 +0000

    feat: data fetched successfully from apis, implementing pagination and data display

--- src/app/features/offers/offers.component.ts
+++ src/app/features/offers/offers.component.ts
@@ -1,11 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Offer } from '../../core/types/offer';
+import { OfferService } from '../../core/services/offer.service';
+import { NgIf, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-offers',
-  imports: [],
+  imports: [NgIf,NgFor],
   templateUrl: './offers.component.html',
   styleUrl: './offers.component.css'
 })
-export class OffersComponent {
+export class OffersComponent implements OnInit, OnDestroy {
+
+  constructor(private offerService: OfferService ){}
+
+  offers: Offer[] = [];
+  currentPage : number = 1;
+  pageSize: number = 20;
+  totalItems: number = 0;
+  isLoading: boolean = false;
+  errorMessage: string = '';
+  searchKey: string = "";
+  country: string = "us";
+  location: string = "";
+
+  fetchOffers(){
+    this.isLoading = true;
+    this.errorMessage = '';
+    
+    this.offerService.getOffers(this.currentPage,this.searchKey,this.country,this.pageSize,this.location).subscribe(
+      {
+        next:(data: Offer[])=>{
+          this.offers = data;
+          this.isLoading = false;
+        },error: (error)=>{
+          this.errorMessage = "Fetching Data Failed";
+          this.isLoading = false;
+          console.log(error);
+        }
+      }
+    )
+  }
+
+  goToNextPage(){
+    this.currentPage++;
+    this.fetchOffers();
+  }
+
+  goToPreviousPage(){
+    if(this.currentPage > 1){
+      this.currentPage--;
+      this.fetchOffers();
+    }
+  }
+
+  ngOnInit(): void {
+    this.fetchOffers();
+  }
+
+  ngOnDestroy(): void {
+    this.offers = [];
+  }
 
 }
