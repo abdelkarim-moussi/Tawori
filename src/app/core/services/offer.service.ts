4057fae5b6ce0b6ce6675e46403d980aec6ac196
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2026-02-10 10:25:10 +0000

    feat: fetch offers data from apis and map results to match Offer structure with error handling

--- src/app/core/services/offer.service.ts
+++ src/app/core/services/offer.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, forkJoin, map, Observable } from 'rxjs';
 import { Offer } from '../types/offer';
 import { environment } from '../../../environments/environment.development';
 
@@ -13,36 +13,62 @@ export class OfferService {
 
   private mapToOffer(data:any,apiSource: string):Offer{
     return {
-        "id": data.id,
+        "id": data.id || data.MatchedObjectId,
         "apiSource": apiSource,
-        "title": data.name || data.title,
-        "company": data.company.display_name || data.company.name,
-        "location": data.location.display_name || data.locations[0].name,
-        "url": data.redirect_url || data.refs.landing_page,
-        "createdAt": data.created || data.publication_date,
+        "title": data.name || data.title || data.PositionTitle,
+        "company": data.company.display_name || data.company.name || data.OrganizationName,
+        "location": data.location.display_name || data.locations[0].name || data.PositionLocationDisplay,
+        "url": data.redirect_url || data.refs.landing_page || data.PositionURI,
+        "createdAt": data.created || data.publication_date || data.PublicationStartDate,
     }
   }
 
-  getOffers(page: number, searchKey: string, country: string, resulsPerPage: number, location: string): Observable<Offer>{
+  getOffers(page: number, searchKey: string, country: string, resulsPerPage: number, location: string): Observable<Offer[]>{
+
+    const halfLimit = Math.floor(resulsPerPage/2);
 
     const adzunaOffers = this.http.get<any>(`${environment.adzunaApiUrl}/${country}/search/${page}`,{
       params : {
         search: searchKey,
         app_id: environment.adzunaAppId,
         app_key: environment.adzunaApiKey,
-        results_per_page: resulsPerPage,
+        results_per_page: halfLimit,
         location0: location
       }
-    });
-    const usajobs = this.http.get<any>(`${environment.usaJobsApiUrl}/Search`,
+    }).pipe(
+      catchError(error => {
+        console.log("Adzuna API Failed: ",error);
+        return [{result: []}]
+      })
+    );
+
+    const usaOffersHeaders = new HttpHeaders({
+      'accept': 'application/json',
+      'Authorization-Key': environment.usaJobsApiKey
+    })
+
+    const usaOffers = this.http.get<any>(`${environment.usaJobsApiUrl}/Search`,
       {
+        headers: usaOffersHeaders,
         params: {
           Page: page,
-          ResultsPerPage : resulsPerPage,
+          ResultsPerPage : halfLimit,
           LocationName : location
         }
       }
+    ).pipe(
+      catchError(error => {
+        console.error("UsaJobs API Failed: ",error);
+        return [{result : []}]
+      })
     );
-    
+
+    return forkJoin([adzunaOffers,usaOffers]).pipe(
+      map(([adzunaRes,usaRes])=>{
+        const adzunaList = (adzunaRes.results || []).map((item:any) => this.mapToOffer(item,"Adzuna"));
+        const usaList = (usaRes.SearchResult?.SearchResultItems || []).map((item:any) => this.mapToOffer(item.MatchedObjectDescriptor,"UsaJobs"));
+
+        return [...adzunaList,...usaList]
+      }))    
   }
 }
