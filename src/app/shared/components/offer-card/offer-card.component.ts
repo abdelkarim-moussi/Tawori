6e313c1c68003e002629c56b844d6413156e5174
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2026-02-11 15:08:39 +0000

    feat: implementing application tracking functionality

--- src/app/shared/components/offer-card/offer-card.component.ts
+++ src/app/shared/components/offer-card/offer-card.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { Offer } from '../../../core/types/offer';
 import { DatePipe } from '@angular/common';
+import { ApplicationApiService } from '../../../core/services/application-api.service';
+import { Application } from '../../../core/types/application';
 
 @Component({
   selector: 'app-offer-card',
@@ -10,4 +12,35 @@ import { DatePipe } from '@angular/common';
 })
 export class OfferCardComponent {
   @Input() offer!: Offer;
+
+  constructor(private applicationService: ApplicationApiService){}
+
+
+  trackApplication(){
+
+    let application: Application = {
+      id:1,
+      userId: 1, 
+      offerId: Number(this.offer.id),
+      apiSource: this.offer.apiSource,
+      title: this.offer.title,
+      company: this.offer.company,
+      location: this.offer.location,
+      url: this.offer.url,
+      status: 'Pending',
+      notes: '',
+      dateAdded: new Date().toISOString()
+    };
+
+    this.applicationService.createApplication(application).subscribe({
+      next:(data:Application)=>{
+        application = data;
+      },
+      error:(error)=>{
+        console.error("There is an Error Will Trying To Save The Application : ",error);
+      }
+    }
+    );
+    
+  }
 }
---
+++ src/app/shared/components/search-filters-header/search-filters-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-search-filters-header',
+  imports: [FormsModule],
+  templateUrl: './search-filters-header.component.html',
+  styleUrl: './search-filters-header.component.css'
+})
+export class SearchFiltersHeaderComponent {
+
+}
