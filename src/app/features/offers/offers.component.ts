7bda835798de9cd50712c3fcea7451de6cc7c166
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2026-02-11 12:01:30 +0000

    refactor: update components style using tailwind css

--- src/app/features/offers/offers.component.ts
+++ src/app/features/offers/offers.component.ts
@@ -2,30 +2,31 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Offer } from '../../core/types/offer';
 import { OfferService } from '../../core/services/offer.service';
 import { NgIf, NgFor, CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { TableComponent } from '../../shared/components/table/table/table.component';
 import { ColumnDef } from '../../core/types/column-def';
 import { OfferCardComponent } from '../../shared/components/offer-card/offer-card.component';
 
 export const COLUMN_DEFINITIONS: ColumnDef<Offer>[] = [
-    { headerText: 'Source', field: 'apiSource' },
-    { headerText: 'Title', field: 'title' },
-    { headerText: 'Company', field: 'company' },
-    { headerText: 'Location', field: 'location'},
-    { headerText: 'Url', field: 'url'},
-    { headerText: 'Publication Date', field: 'createdAt'}
+  { headerText: 'Source', field: 'apiSource' },
+  { headerText: 'Title', field: 'title' },
+  { headerText: 'Company', field: 'company' },
+  { headerText: 'Location', field: 'location' },
+  { headerText: 'Url', field: 'url' },
+  { headerText: 'Publication Date', field: 'createdAt' }
 ];
 @Component({
   selector: 'app-offers',
-  imports: [NgIf,NgFor, TableComponent, OfferCardComponent,CommonModule],
+  imports: [NgIf, NgFor, TableComponent, OfferCardComponent, CommonModule, FormsModule],
   templateUrl: './offers.component.html',
   styleUrl: './offers.component.css'
 })
 export class OffersComponent implements OnInit, OnDestroy {
 
-  constructor(private offerService: OfferService ){}
+  constructor(private offerService: OfferService) { }
 
   offers: Offer[] = [];
-  currentPage : number = 1;
+  currentPage: number = 1;
   pageSize: number = 20;
   totalItems: number = 0;
   isLoading: boolean = false;
@@ -35,16 +36,16 @@ export class OffersComponent implements OnInit, OnDestroy {
   location: string = "";
   columns = COLUMN_DEFINITIONS;
 
-  fetchOffers(){
+  fetchOffers() {
     this.isLoading = true;
     this.errorMessage = '';
-    
-    this.offerService.getOffers(this.currentPage,this.searchKey,this.country,this.pageSize,this.location).subscribe(
+
+    this.offerService.getOffers(this.currentPage, this.searchKey, this.country, this.pageSize, this.location).subscribe(
       {
-        next:(data: Offer[])=>{
+        next: (data: Offer[]) => {
           this.offers = data;
           this.isLoading = false;
-        },error: (error)=>{
+        }, error: (error) => {
           this.errorMessage = "Fetching Data Failed";
           this.isLoading = false;
           console.log(error);
@@ -53,16 +54,21 @@ export class OffersComponent implements OnInit, OnDestroy {
     )
   }
 
-  goToNextPage(){
+  goToNextPage() {
     this.currentPage++;
     this.fetchOffers();
   }
 
-  goToPreviousPage(){
-    if(this.currentPage > 1){
+  goToPreviousPage() {
+    if (this.currentPage > 1) {
       this.currentPage--;
       this.fetchOffers();
     }
+  }
+
+  onSearch() {
+    this.currentPage = 1;
+    this.fetchOffers();
   }
 
   ngOnInit(): void {
--- src/app/shared/components/offer-card/offer-card.component.ts
+++ src/app/shared/components/offer-card/offer-card.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { Offer } from '../../../core/types/offer';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-offer-card',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './offer-card.component.html',
   styleUrl: './offer-card.component.css'
 })
