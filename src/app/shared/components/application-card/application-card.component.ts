c23ba7ed75c134f41cb10d3884e0a7b640f80bc3
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2026-02-13 10:20:46 +0000

    feat: implement withdraw(delete) application functionalit

--- src/app/shared/components/application-card/application-card.component.ts
+++ src/app/shared/components/application-card/application-card.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { Application } from '../../../core/types/application';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { ApplicationApiService } from '../../../core/services/application-api.service';
 @Component({
   selector: 'app-application-card',
   imports: [CommonModule,RouterLink],
@@ -11,4 +12,22 @@ import { RouterLink } from '@angular/router';
 export class ApplicationCardComponent {
   @Input() application!: Application;
 
+  messageType: 'success' | 'error' | null = null;
+  message: string = '';
+
+  constructor(private applicationService: ApplicationApiService){}
+
+
+  deleteApplication(applicationId: number){
+    this.applicationService.deleteApplication(applicationId).subscribe({
+      next:(data)=>{
+        this.messageType = "success";
+        this.message = "application withdrawed succefully"
+      },
+      error:(error)=>{
+        this.messageType = "error";
+        this.message = "Error Withdrawing This Application"
+      }
+    });
+  }
 }
